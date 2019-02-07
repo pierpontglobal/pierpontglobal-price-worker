c86bfeca44b8ff1ac78d933c3e0bae6abdfc7264
root <root@ip-172-31-0-168.ec2.internal>
2019-02-07 13:42:58 +0000

    Server compatible

--- PriceWorker.py
+++ PriceWorker.py
@@ -28,6 +28,8 @@ class PriceWorker:
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--window-size=1920,1080')
+	    chrome_options.add_argument('--no-sandbox')
+	    chrome_options.add_argument('--disable-dev-shm-usage')
             self.driverSession = webdriver.Chrome('./chromedriver',chrome_options=chrome_options)
             self.driverSession.get("https://www.manheim.com/login?WT.svl=m_uni_hdr")
 
