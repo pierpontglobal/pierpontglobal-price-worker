053978622e0e315c72f351284129282049f2e9be
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-02-07 12:35:38 -0400

    Fixed re-initialization after fail

--- PriceWorker.py
+++ PriceWorker.py
@@ -23,16 +23,17 @@ class PriceWorker:
             time.sleep(random.randint(1,2)/70.0)
             element.send_keys(char)
 
-    def get_mmr(self, data_message, ws):
-        if self.driverSession is None:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--window-size=1920,1080')
-	    chrome_options.add_argument('--no-sandbox')
-	    chrome_options.add_argument('--disable-dev-shm-usage')
-            self.driverSession = webdriver.Chrome('./chromedriver',chrome_options=chrome_options)
-            self.driverSession.get("https://www.manheim.com/login?WT.svl=m_uni_hdr")
+    def send_response(self, response, ws, vin, id):
+        response_data = {}
+        response_data["command"] = 'message'
+        response_data["identifier"] = "{\"channel\": \"PriceQueryChannel\"}"
+        response_data["data"] = "{\"action\": \"update_status\", \"vin\": \""+ vin +"\", \"mmr\": \""+ response +"\", \"user\": \""+ str(id) +"\"}"
+        response_json = json.dumps(response_data)
+        print response_json
+        ws.send(response_json)
 
+
+    def login(self):
             usernameElement = self.driverSession.find_element_by_id("user_username")
             passwordElement = self.driverSession.find_element_by_id("user_password")
             loginbutton = self.driverSession.find_element_by_name("submit")
@@ -40,9 +41,31 @@ class PriceWorker:
             self.human_type(usernameElement, 'pierpontllc')
             self.human_type(passwordElement, 'Kittie123!')
             loginbutton.click()
-        
+
+    def get_mmr(self, data_message, ws, attempt = 0):
+        if self.driverSession is None:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--window-size=1920,1080')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            self.driverSession = webdriver.Chrome('./chromedriver',chrome_options=chrome_options)
+            self.driverSession.get("https://www.manheim.com/login?WT.svl=m_uni_hdr")
+
         VIN = data_message['vin']
-        timeout = 5
+        USER_ID = data_message['user_id']
+
+        login_presence = (len(self.driverSession.find_elements_by_css_selector('#user_username')) > 0)
+        if (login_presence and attempt < 3):
+            print 'Login attempt ' + str(attempt)
+            self.login()
+            self.get_mmr(data_message, ws, attempt + 1)
+            return
+        elif attempt >= 3:
+            self.send_response("Not available", ws, VIN, USER_ID)
+            return
+        
+        timeout = 2
         element_present = EC.presence_of_element_located((By.NAME, 'searchTerms'))
         WebDriverWait(self.driverSession, timeout).until(element_present)
 
@@ -51,15 +74,11 @@ class PriceWorker:
         time.sleep(random.randint(1,2)/30.0)
         searchElement.send_keys(Keys.ENTER)
 
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'mmr-valuation'))
-        WebDriverWait(self.driverSession, timeout).until(element_present)
-
-        mmrElement = self.driverSession.find_element_by_class_name("mmr-valuation").find_element_by_tag_name('a').text
-
-        response_data = {}
-        response_data["command"] = 'message'
-        response_data["identifier"] = "{\"channel\": \"PriceQueryChannel\"}"
-        response_data["data"] = "{\"action\": \"update_status\", \"vin\": \""+ data_message['vin'] +"\", \"mmr\": \""+ mmrElement +"\", \"user\": \""+ str(data_message['user_id']) +"\"}"
-        response_json = json.dumps(response_data)
-        print response_json
-        ws.send(response_json)
+        try:
+            print 'Query attempt ' + str(attempt)
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'mmr-valuation'))
+            WebDriverWait(self.driverSession, timeout).until(element_present)
+            mmrElement = self.driverSession.find_element_by_class_name("mmr-valuation").find_element_by_tag_name('a').text
+            self.send_response(mmrElement, ws, VIN, USER_ID)
+        except:
+            self.get_mmr(data_message, ws, attempt + 1)
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def on_message(ws, message):
         print data['message']
         if (action == "query_mmr"):
             priceWorker.get_mmr(data_message, ws)
-    except Exception,e: print str(e)
+    except Exception,e: None
 
 def on_error(ws, error):
     print(error)
@@ -30,7 +30,7 @@ def on_open(ws):
         data["command"] = "subscribe"
         data["identifier"] = "{\"channel\": \"PriceQueryChannel\"}"
         string_data = json.dumps(data)
-        print string_data
+        print 'Connecting to: ' + string_data
         ws.send(string_data)
     thread.start_new_thread(run, ())
 
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     priceWorker = PriceWorker.PriceWorker()
 
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://api.pierpontglobal.com/cable?token=83fceb76a12aa752414ef88bebeae6dca65f41108b2b4845bc344764e1c8df8f",
+    ws = websocket.WebSocketApp("wss://api.pierpontglobal.com/cable?token=63dfd03b8c2a3bab112495489c64dcf92205a9d73449b3ca5c02132e6d08ddee",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
